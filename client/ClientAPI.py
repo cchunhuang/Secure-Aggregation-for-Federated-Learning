806f84b87b1ea5bbd293060ad9b0140adf2798a7
Noah <103417423+Lunorcia@users.noreply.github.com>
2024-12-12 05:23:43 +0800

    feat: ClientAPI and dropOutHandler update
    add func in ClientAPI to access client ID
    add param round_number in ClientAPI.clientUpdate()
    dropOutHandler hasn't completed yet

--- client/ClientAPI.py
+++ client/ClientAPI.py
@@ -6,6 +6,20 @@ class ClientAPI:
         Initialize the client API
         '''
         self.client = Client()
+    def setClientID(self, c_id):
+        """
+        Set ID got from server
+
+        Parameters:
+            c_id (int): client's new ID
+        """
+        self.client.client_id = c_id
+        
+    def getClientID(self):
+        """
+        return client ID
+        """
+        return self.client.client_id
     
     def uploadPublicKey(self):
         '''
@@ -31,19 +45,20 @@ class ClientAPI:
         self.client.downLoadPublicKey(keys)
         return
     
-    def clientUpdate(self, model, selected_clients:list):
+    def clientUpdate(self, model, selected_clients:list, round_number):
         '''
         Update the client model
         
         Parameters:
             model (torch.nn.Module): Model to update
             selected_clients (list of int): List of selected clients
+            round_number (int): Current round number
             
         Returns:
             torch.nn.Module: Updated model
             or None: if the client drop out
         '''
-        return self.client.clientUpdate(model, selected_clients)
+        return self.client.clientUpdate(model, selected_clients, round_number)
     
     def dropOutHanlder(self, model, selected_clients:list):
         '''
--- server/Server.py
+++ server/Server.py
@@ -4,11 +4,29 @@ from client import ClientAPI
 
 class Server:
     def __init__(self):
-        self.public_keys = {}  # all clients' public keys
+        """Initialize Server
+        all_clients (dict): key (int): client ID
+                            value (ClientAPI): client info
+
+        """
+        self.all_clients = {}
+        self.all_public_keys = {}  # all clients' public keys
         self.global_model = None
         self.updates = {}
-        self.selected_clients = []  # clients selected in current round
-        self.dropout_clients = []
+        self.selected_clients = []  # clients (ID) selected in current round
+        self.dropout_clients = []  # drop-out clients (ID)
+        self.round_number = 1
+        self.current_id = 1  # ID distributed to new client
+        self.prime = 1  # prime for compute blinding factor
+
+    def registerClient(self, client_api: ClientAPI):
+        """
+        Register new client and upload its public key to server
+        """
+        client_api.setClientID(self.current_id)
+        self.all_clients[client_api.getClientID()] = client_api
+        self.receivePublicKey(client_api.getClientID(), client_api.uploadPublicKey())
+        self.current_id += 1
 
     def initGlobalModel(self, model_size):
         """
@@ -18,42 +36,44 @@ class Server:
         self.global_model = np.random.rand(model_size)
         return
 
-    def getPublicKey(self, client_id, public_key):
+    def receivePublicKey(self, client_id, public_key):
         """
-        receive public key from client
+        Receive public key from client
 
         Parameters:
             client_id (int)
-            public_key (int): from ClientAPI.uploadPublicKey()
+            public_key (int)
         """
-        self.public_keys[client_id] = public_key
+        self.all_public_keys[client_id] = public_key
 
     def distributePublicKey(self, client_id):
         """
-        send other clients' public key
+        Send other clients' public key
 
         Parameters:
             client_id (int): current client request others public key
 
         Returns:
             dict: other clients' public key
+                key (int): client ID
+                value (int): public keys
         """
-        return {k: v for k, v in self.public_keys.items() if k != client_id}
+        return {k: v for k, v in self.all_public_keys.items() if k != client_id}
 
-    def distributeGlobalModel(self):
-        """
-        distribute global model to selected clients
+    # def distributeGlobalModel(self):
+    #     """
+    #     Distribute global model to selected clients
 
-        Returns:
-            dict:
-                key (int): client id
-                value (int): global model
-        """
-        return {client_id: self.global_model for client_id in self.selected_clients}
+    #     Returns:
+    #         dict:
+    #             key (int): client ID
+    #             value (int): global model
+    #     """
+    #     return {client_id: self.global_model for client_id in self.selected_clients}
 
-    def getClientUpdate(self, client_id, client_update):
+    def receiveClientUpdate(self, client_id, client_update):
         """
-        get update from client
+        Save update from client
 
         Parameters:
             client_id (int)
@@ -62,12 +82,25 @@ class Server:
         self.updates[client_id] = client_update
 
     def dropOutHandler(self):
-        for client in self.dropout_clients:
-            if client in self.selected_clients:
-                self.updates[client] = np.zeros_like(
-                    self.global_model
-                )  # replace by zero vec
-        return self.updates
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        q_vectors = []
+        for client_id in self.selected_clients:
+            if client_id not in self.dropout_clients:
+                # send drop-out list to the client online
+                # online client compute blinding_factors and return
+                q_vec = self.all_clients[client_id].dropOutHanlder()
+                q_vectors.append(q_vec)
+        # compute final q vector
+        q = np.sum(q_vectors, axis=0) % self.prime
+        # adjust aggregate result
+        corrected_model_update = (
+            np.sum(self.updates.values(), axis=0) - q
+        ) % self.prime
+        return corrected_model_update
 
     def aggregateUpdate(self):
         """
@@ -82,43 +115,57 @@ class Server:
 
     def deQuantizeUpdate(self, quantized_update, scale_factor):
         """
-
-        Args:
-            quantized_update (np.ndarray): quantized model update
+        Dequantize update from int to float
+        Parameters:
+            quantized_update (np.ndarray): quantized(aggregated) model update
             scale_factor (float)
+
+        Returns:
+            np.ndarray: (float) aggregated model
         """
         return quantized_update / scale_factor
 
     def updateGlobalModel(self, aggregated_update):
         """
-
-        Args:
+        Parameters:
             aggregated_update (np.ndarray): from aggregateUpdate
         """
         self.global_model += aggregated_update
 
-    def runRound(self, selected_clients, scale_factor):
+    def selectClients(self):
         """
-        update selected clients and run a round
+        Select clients to join next round
+        Update selected_clients(list of client ID)
+        """
+        pass
 
-        Args:
-            selected_clients (list): selected clients in this round
+    def runRound(self, scale_factor):
+        """
+        Update selected clients and run a round
+
+        Parameters:
             scale_factor (float): factor used in dequantize
         """
-        self.selected_clients = selected_clients
+        self.selected_clients = self.selectClients()
 
         # 1. distribute global model
-        global_model_mapping = self.distributeGlobalModel()
         # 2. receive clients update
-        for client_id in selected_clients:
-
-            client_update = None  # call clientAPI.clientUpdate()
-            self.getClientUpdate(client_id, client_update)
+        for client_id in self.selected_clients:
+            client_api = self.all_clients[client_id]
+            client_update = client_api.clientUpdate(
+                model=self.global_model,
+                selected_clients=self.selected_clients,
+                round_number=self.round_number,
+            )  # if client drop-out, receive None
+            if client_update is None:
+                self.dropout_clients.append(client_id)
+            self.receiveClientUpdate(client_id, client_update)
         # 3. drop-out handling
-        self.dropOutHandler()
+        corrected_model_update = self.dropOutHandler()
         # 4. aggregate update
         aggregated_update = self.aggregateUpdate()
         # 5. dequantize update
         dequantized_update = self.deQuantizeUpdate(aggregated_update, scale_factor)
         # 6. update global model
         self.updateGlobalModel(dequantized_update)
+        self.round_number += 1
